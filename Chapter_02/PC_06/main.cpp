1ed33a3b6587ca1b5bdee34fd2c175a3eba1a380
yasojan <yasinzahir98@gmail.com>
2026-04-13 20:59:45 -0700

    Finish Pset #6

--- Chapter_02/PC_06/main.cpp
+++ Chapter_02/PC_06/main.cpp
@@ -1,7 +1,26 @@
 /**************************************************************************************************** 
 * Author: Yasin Zahir
 
-* Average of Values
+* Annual Pay
 
 * 4/13/2026
 *****************************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+
+int main(){
+  int pay_amount = 2200;
+  const int PAY_PERIODS = 26;
+  int annual_pay = pay_amount * PAY_PERIODS;
+
+  cout << "-------------- Annual Pay --------------" << endl;
+  cout << fixed << setprecision(2);
+  cout << "Total annual pay: $" << annual_pay << endl;
+  cout << "-------------- Annual Pay --------------" << endl;
+  return 0;
+}
